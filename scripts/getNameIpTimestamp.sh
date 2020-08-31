f5047c26964be6fdb944e0cefed962f5fe8a995b
MarceHM <47231554+MarceHM@users.noreply.github.com>
2020-08-30 19:18:53 -0500

    Update getNameIpTimestamp.sh

--- scripts/getNameIpTimestamp.sh
+++ scripts/getNameIpTimestamp.sh
@@ -3,4 +3,4 @@
 #This script prints the hostname, the assigned ip address and the current timestamp.
 echo "The hostname is" $HOSTNAME
 echo "The ip address is" $(hostname -I | awk '{print $2}')
-echo "The current timestamp is" $(date '+%s')
+echo "The current timestamp is" $(date)
