dabbe884c6fc8bc1973bbd21aa39962026ef8684
DanielCerquera13 <danielalejandrocerquera@gmail.com>
2021-02-14 18:23:02 -0500

    Edit server.sh

--- server.sh
+++ server.sh
@@ -2,9 +2,6 @@ sudo yum install -y wget
 sudo yum install -y httpd
 cd /var/www/html
 wget www.icesi.edu.co
-sudo chmod 777 /etc/httpd/conf/httpd.conf
-cd /vagrant
-cp httpd.conf /etc/httpd/conf
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 
