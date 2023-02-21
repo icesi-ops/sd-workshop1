70fcafb6797c5143d4505c83f546ea8e85587743
Valentina Arias Parra <54784992+ValeArias07@users.noreply.github.com>
2023-02-21 01:06:03 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 $script = "default-lease-time 600;
 max-lease-time 7200; 
 authoritative;
-subnet 192.168.20.0 netmask 255.255.255.0 { 
-	range 192.168.20.100 192.168.20.200;
+subnet 192.168.56.0 netmask 255.255.255.0 { 
+	range 192.168.56.100 192.168.56.200;
 	option domain-name-servers 8.8.8.8;
 	option domain-name 'server.com';
-	option routers 192.168.20.254; 
-	option broadcast-address 192.168.20.255; 
+	option routers 192.168.56.254; 
+	option broadcast-address 192.168.56.255; 
 }"
 
 apt-get update
